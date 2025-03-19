a172aa10459f6a193e8173f0a5c80e91b2bb9408
Aditya Ekka <adityaekka2003@gmail.com>
2025-03-19 20:08:29 +0530

    Add server

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -19,7 +19,7 @@ const App = () => {
 
   async function reviewCode() {
     try {
-      const response = await axios.post("http://localhost:3000/ai/get-review", {
+      const response = await axios.post("https://aicodereviewer-8csu.onrender.com/ai/get-review", {
         code,
       });
       setReview(response.data);
